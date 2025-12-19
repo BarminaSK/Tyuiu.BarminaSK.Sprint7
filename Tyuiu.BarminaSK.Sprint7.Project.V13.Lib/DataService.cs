7c05372883cb67161a5dca0703fea81a6fdf06c8
BarminaSK <barminask@mail.ru>
2025-12-19 16:47:17 +0500

    Edit3

--- Tyuiu.BarminaSK.Sprint7.Project.V13.Lib/DataService.cs
+++ Tyuiu.BarminaSK.Sprint7.Project.V13.Lib/DataService.cs
@@ -1,7 +1,78 @@
-﻿namespace Tyuiu.BarminaSK.Sprint7.Project.V13.Lib
-{
-    public class DataService
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
+namespace Tyuiu.BarminaSK.Sprint7.Project.V13.Lib
+{
+    public class Country_BSK
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public double Area { get; set; }
+        public bool IsDeveloped { get; set; }
+        public long Population { get; set; }
+        public string MainNationality { get; set; }
+        public string Note { get; set; }
+
+        public Country_BSK() { }
+
+        public Country_BSK(string name, string capital, double area,
+                          bool isDeveloped, long population,
+                          string mainNationality, string note)
+        {
+            Name = name;
+            Capital = capital;
+            Area = area;
+            IsDeveloped = isDeveloped;
+            Population = population;
+            MainNationality = mainNationality;
+            Note = note;
+        }
+    }
+
+    public class DataService_BSK
+    {
+        public List<Country_BSK> LoadFromCsvFile(string FilePath)
+        {
+            List<Country_BSK> countries = new List<Country_BSK>();
+
+            string[] allLines = File.ReadAllLines(FilePath);
+
+            for (int i = 1; i < allLines.Length; i++)
+            {
+                // 4. РАЗБИВАЕМ СТРОКУ ПО ЗАПЯТЫМ
+                string[] columns = allLines[i].Split(',');
+
+                Country_BSK country = new Country_BSK();
+
+                country.Name = columns[0];
+                country.Capital = columns[1];
+                country.Area = double.Parse(columns[2]);
+                country.IsDeveloped = bool.Parse(columns[3]);
+                country.Population = long.Parse(columns[4]);
+                country.MainNationality = columns[5];
+                country.Note = columns[6];
+
+                countries.Add(country);
+            }
+            return countries;
+        }
+        public void SaveToCsvFile(List<Country_BSK> countries, string filePath)
+        {
+            List<string> lines = new List<string>();
+
+            lines.Add("Name,Capital,Area,IsDeveloped,Population,MainNationality,Note");
+
+            foreach (Country_BSK country in countries)
+            {
+                string line = $"{country.Name},{country.Capital},{country.Area}," +
+                             $"{country.IsDeveloped},{country.Population}," +
+                             $"{country.MainNationality},{country.Note}";
+
+                lines.Add(line);
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
     }
 }
--- Tyuiu.BarminaSK.Sprint7.Project.V13.Test/DataServiceTest.cs
+++ Tyuiu.BarminaSK.Sprint7.Project.V13.Test/DataServiceTest.cs
@@ -6,8 +6,54 @@ namespace Tyuiu.BarminaSK.Sprint7.Project.V13.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void ValidCheck()
+        public void ValidLoadFromCsvFile()
         {
+            DataService_BSK ds = new DataService_BSK();
+
+            string path = @"C:\DataSprint7\countries.csv";
+
+            List<Country_BSK> result = ds.LoadFromCsvFile(path);
+
+            Assert.AreEqual(10, result.Count);
+
+            Assert.AreEqual("Россия", result[0].Name);
+            Assert.AreEqual("Москва", result[0].Capital);
+            Assert.AreEqual(17100000, result[0].Area);
+            Assert.AreEqual(true, result[0].IsDeveloped);
+            Assert.AreEqual(146000000, result[0].Population);
+            Assert.AreEqual("Русские", result[0].MainNationality);
+            Assert.AreEqual("Самая большая страна", result[0].Note);
+        }
+        [TestMethod]
+        public void ValidSaveToCsvFile()
+        {
+            DataService_BSK ds = new DataService_BSK();
+
+            List<Country_BSK> testData = new List<Country_BSK>
+            {
+                new Country_BSK("Тестовая1", "Столица1", 100000, true, 50000000, "Нация1", "Примечание1"),
+                new Country_BSK("Тестовая2", "Столица2", 200000, false, 70000000, "Нация2", "Примечание2")
+            };
+
+            string testFilePath = @"test_output.csv";
+
+            ds.SaveToCsvFile(testData, testFilePath);
+
+            Assert.IsTrue(File.Exists(testFilePath));
+
+            List<Country_BSK> loadedData = ds.LoadFromCsvFile(testFilePath);
+
+            Assert.AreEqual(2, loadedData.Count);
+
+            Assert.AreEqual("Тестовая1", loadedData[0].Name);
+            Assert.AreEqual("Столица1", loadedData[0].Capital);
+            Assert.AreEqual(100000, loadedData[0].Area);
+            Assert.AreEqual(true, loadedData[0].IsDeveloped);
+            Assert.AreEqual(50000000, loadedData[0].Population);
+            Assert.AreEqual("Нация1", loadedData[0].MainNationality);
+            Assert.AreEqual("Примечание1", loadedData[0].Note);
+
+            File.Delete(testFilePath);
         }
     }
 }
